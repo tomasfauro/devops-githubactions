3287f01a33d651e4911a06e65251ed5973512e60
Tomas Fauro <tomyfauro@gmail.com>
2025-10-17 00:37:56 +0200

    init: proyecto python con tests

---
+++ main.py
@@ -0,0 +1,5 @@
+import os 
+
+class calculator:
+    def sum(self, a: int, b: int) -> int:
+        return 0
---
+++ test.py
@@ -0,0 +1,4 @@
+from main import calculator
+
+def test_sums_2_numbers():
+    assert calculator().sum(2, 2) == 4
